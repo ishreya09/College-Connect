f06e1230279edfe9a7e879fee86d5e318a1c1cf6
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-10-16 23:43:05 +0530

    Make models of student, mentor, branch, department, contact us form

--- CollegeConnect/branch/models.py
+++ CollegeConnect/branch/models.py
@@ -3,4 +3,9 @@ from django.db import models
 # Create your models here.
 class Department(models.Model):
     department_name= models.CharField(max_length=500,null=True, blank= True)
-    department_id= models.CharField()
+    department_id= models.BigAutoField(primary_key=True, auto_created=True)
+
+class Branch(models.Model):
+    branch_name = models.CharField(max_length=200, null=True, blank=True)
+    branch_code= models.BigAutoField(primary_key=True, auto_created=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='branches') # one to many
--- CollegeConnect/home/models.py
+++ CollegeConnect/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ContactUs(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    query= models.TextField()
+    sent_on= models.DateTimeField(auto_now_add=True)
