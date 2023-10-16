4092321ff5615e191807d806bbd39e0c40d51c57
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-10-16 16:24:47 +0530

    Add contact us

--- CollegeConnect/home/urls.py
+++ CollegeConnect/home/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-    path('', views.home,name= 'home')
+    path('', views.home,name= 'home'),
+    path('privacy-policy',views.privacypolicy,name="privacypolicy"),
+    path('contact-us',views.contact_us,name="contactus"),
 ]
 
 
--- CollegeConnect/home/views.py
+++ CollegeConnect/home/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def home(request):
     context={}
     return render(request, 'home/home.html', context)
+
+def privacypolicy(request):
+    context = {}
+    return render(request, 'home/privacypolicy.html', context)
+
+def contact_us(request):
+    context={}
+    return render (request,'home/contactus.html',context)
+
