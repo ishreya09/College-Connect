d1d707dc67f754072c203ddb2da867bce22a7274
shreeja <shreejar197@gmail.com>
2023-10-25 11:54:14 +0530

    storing the contactus queries

--- CollegeConnect/home/views.py
+++ CollegeConnect/home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import ContactUs
 # Create your views here.
 def home(request):
     context={}
@@ -10,6 +10,21 @@ def privacypolicy(request):
     return render(request, 'home/privacypolicy.html', context)
 
 def contact_us(request):
+    if request.method == 'POST':
+        name = request.POST.get('name',False)
+        email = request.POST.get('email',False)
+        query = request.POST.get('query',False)
+        if not name:
+            messages.error(request, 'Name is required.')
+        if not email:
+            messages.error(request, 'Email is required.')
+        if not query:
+            messages.error(request, 'Query is required.')
+        if name and email and query:
+            ContactUs.objects.create(name=name, email=email, query=query)
+            return redirect ('/contact-success')
+        else:
+            return redirect('/contact-us')
     context={}
     return render (request,'home/contactus.html',context)
 
