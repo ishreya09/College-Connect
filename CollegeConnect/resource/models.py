e1878578df4202b17fc9d7d0b61e7d4a9d6d33ca
shreeja <shreejar197@gmail.com>
2023-11-03 23:40:44 +0530

    model for resources file upload

--- CollegeConnect/resource/models.py
+++ CollegeConnect/resource/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from multiupload.fields import MultiFileField
+from taggit.managers import TaggableManager
+from branch.models import Branch
 
-from branch.models import Department, Branch
+def upload_to(instance, filename):
+    return 'resources/{}/{}'.format(instance.branch.branch_name, filename)
 
 class UploadedFile(models.Model):
     title = models.CharField(max_length=255)
-    file = models.FileField(upload_to='uploads/')
+    files = MultiFileField(upload_to=upload_to)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='uploaded_files', null=True, blank=True)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='uploaded_files', null=True, blank=True)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
-
-
-
