b2d3766ffe00e51aa8edc510e953f3ea7bb9d7e1
shreeja <shreejar197@gmail.com>
2023-11-03 23:04:08 +0530

    model for resources file upload

--- CollegeConnect/resource/models.py
+++ CollegeConnect/resource/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from branch.models import Department, Branch
+
+class UploadedFile(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='uploaded_files', null=True, blank=True)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='uploaded_files', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
