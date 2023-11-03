93966955d086a1ab2f9b50024220fec241a3a683
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-11-04 00:18:49 +0530

    add mentor mail feature on approval and removal working and resource model fixed

--- CollegeConnect/account/models.py
+++ CollegeConnect/account/models.py
@@ -58,22 +58,33 @@ class Mentor(models.Model):
             s.is_mentor=False
             s.save()
     def send_approval_mail(self):
-        seven_day_ago=timezone.now() - timezone.timedelta(days=7)
+        # seven_day_ago=timezone.now() - timezone.timedelta(days=7)
         if self.approved:
             u=User.objects.get(username=self.username)
+            print("hello")
             email_from = settings.EMAIL_HOST_USER
             subject = 'Mentor Application Approved'
             message = 'Congratulations '+ u.first_name+" "+ u.last_name +'!! Your mentor application has been approved. You can now mentor students at our platform College Connect.'
             recipient_list = [self.student.college_email,u.email ]
-            send_mail( subject, message, email_from, recipient_list )
+            # send_mail( subject, message, email_from, recipient_list ,fail_silently=False)
+            # u.email_user(subject,message)
             # elif self.last_application_date> seven_day_ago:
+            try:
+                send_mail(subject, message, email_from, recipient_list, fail_silently=False)
+            except Exception as e:
+                print(f"Email sending failed: {e}")
         else:
             u=User.objects.get(username=self.username)
             email_from = settings.EMAIL_HOST_USER
             subject = 'Removed as Mentor'
             message = 'Sorry '+ u.first_name+" "+ u.last_name +', You can no longer mentor students at our platform College Connect.'
             recipient_list = [self.student.college_email,u.email ]
-            send_mail( subject, message, email_from, recipient_list )
+            # send_mail( subject, message, email_from, recipient_list )
+            # u.email_user(subject,message)
+            try:
+                send_mail(subject, message, email_from, recipient_list, fail_silently=False)
+            except Exception as e:
+                print(f"Email sending failed: {e}")
 
             
   
--- CollegeConnect/resource/models.py
+++ CollegeConnect/resource/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-from multiupload.fields import MultiFileField
 from taggit.managers import TaggableManager
 from branch.models import Branch
 
 def upload_to(instance, filename):
     return 'resources/{}/{}'.format(instance.branch.branch_name, filename)
 
-class UploadedFile(models.Model):
+class Resource(models.Model):
     title = models.CharField(max_length=255)
-    files = MultiFileField(upload_to=upload_to)
+    files = models.FileField(upload_to='resource/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='uploaded_files', null=True, blank=True)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='branch', null=True, blank=True)
     tags = TaggableManager()
 
     def __str__(self):
